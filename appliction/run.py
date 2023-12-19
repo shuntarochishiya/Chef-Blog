7532e835423683c51748c8b98f521038f9f20e6e
Kazakevich Tatiana <sasukeutiha719@gmail.com>
2023-12-19 16:24:08 +0300

    no comments

--- appliction/run.py
+++ appliction/run.py
@@ -1,10 +1,8 @@
 from application import create_app, db
-from no_orm import create_tables
 
 app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    create_tables()
     app.run(debug=True)
